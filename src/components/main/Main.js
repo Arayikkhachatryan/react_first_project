f32d13185cab9f25ae4de90e449bb17f7e10c238
arayikkhachatryan <solnce223@gmail.com>
2022-07-13 20:27:50 +0400

    edited

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,55 +1,54 @@
 import './mainStyle.css'
 
-
 function Main() {
     return (
-    <div id="content">
-        <div className="banner" id="home"></div>
-        <div className="about-area" id="about">
-            <div className="info">
-                <h1>About</h1>
-                <p className='info-p'>    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente adipisci soluta at itaque, aspernatur minus
-                    numquam, vel fuga facere tempora eius dolorem quibusdam iste eligendi dolore? Natus quis ut aut? Lorem ipsum dolor
-                    sit amet consectetur adipisicing elit. Aspernatur quisquam dolore est alias, reprehenderit voluptate ipsa, non quo
-                    quaerat dicta debitis! Ea fugit id veritatis dolore. Consequuntur veniam assumenda ab? Lorem ipsum, dolor sit amet
-                    consectetur adipisicing elit. Aperiam sequi autem rerum alias totam aliquid explicabo nam? A autem quibusdam
-                    suscipit praesentium cum, doloribus sit mollitia quae dicta. Tempore, ut.</p>
+        <div id="content">
+            <div className="banner" id="home"></div>
+            <div className="about-area" id="about">
+                <div className="info">
+                    <h1>About</h1>
+                    <p className='info-p'>    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente adipisci soluta at itaque, aspernatur minus
+                        numquam, vel fuga facere tempora eius dolorem quibusdam iste eligendi dolore? Natus quis ut aut? Lorem ipsum dolor
+                        sit amet consectetur adipisicing elit. Aspernatur quisquam dolore est alias, reprehenderit voluptate ipsa, non quo
+                        quaerat dicta debitis! Ea fugit id veritatis dolore. Consequuntur veniam assumenda ab? Lorem ipsum, dolor sit amet
+                        consectetur adipisicing elit. Aperiam sequi autem rerum alias totam aliquid explicabo nam? A autem quibusdam
+                        suscipit praesentium cum, doloribus sit mollitia quae dicta. Tempore, ut.</p>
+                </div>
+            </div>
+            <div className="portfolio-area" id="portfolio">
+                <div className="info">
+                    <h1>Portfolio</h1>
+                    <p className='info-p'>    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente adipisci soluta at itaque, aspernatur minus
+                        numquam, vel fuga facere tempora eius dolorem quibusdam iste eligendi dolore? Natus quis ut aut? Lorem ipsum dolor
+                        sit amet consectetur adipisicing elit. Aspernatur quisquam dolore est alias, reprehenderit voluptate ipsa, non quo
+                        quaerat dicta debitis! Ea fugit id veritatis dolore. Consequuntur veniam assumenda ab? Lorem ipsum, dolor sit amet
+                        consectetur adipisicing elit. Aperiam sequi autem rerum alias totam aliquid explicabo nam? A autem quibusdam
+                        suscipit praesentium cum, doloribus sit mollitia quae dicta. Tempore, ut.</p>
+                </div>
+            </div>
+            <div className="sercvices-area" id="sercvices">
+                <div className="info">
+                    <h1>Services</h1>
+                    <p className='info-p'>    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente adipisci soluta at itaque, aspernatur minus
+                        numquam, vel fuga facere tempora eius dolorem quibusdam iste eligendi dolore? Natus quis ut aut? Lorem ipsum dolor
+                        sit amet consectetur adipisicing elit. Aspernatur quisquam dolore est alias, reprehenderit voluptate ipsa, non quo
+                        quaerat dicta debitis! Ea fugit id veritatis dolore. Consequuntur veniam assumenda ab? Lorem ipsum, dolor sit amet
+                        consectetur adipisicing elit. Aperiam sequi autem rerum alias totam aliquid explicabo nam? A autem quibusdam
+                        suscipit praesentium cum, doloribus sit mollitia quae dicta. Tempore, ut.</p>
+                </div>
+            </div>
+            <div className="contact-area" id="contact">
+                <div className="info">
+                    <h1>Contanct</h1>
+                    <p className='info-p'>    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente adipisci soluta at itaque, aspernatur minus
+                        numquam, vel fuga facere tempora eius dolorem quibusdam iste eligendi dolore? Natus quis ut aut? Lorem ipsum dolor
+                        sit amet consectetur adipisicing elit. Aspernatur quisquam dolore est alias, reprehenderit voluptate ipsa, non quo
+                        quaerat dicta debitis! Ea fugit id veritatis dolore. Consequuntur veniam assumenda ab? Lorem ipsum, dolor sit amet
+                        consectetur adipisicing elit. Aperiam sequi autem rerum alias totam aliquid explicabo nam? A autem quibusdam
+                        suscipit praesentium cum, doloribus sit mollitia quae dicta. Tempore, ut.</p>
+                </div>
             </div>
         </div>
-        <div className="portfolio-area" id="portfolio">
-            <div className="info">
-                <h1>Portfolio</h1>
-                <p className='info-p'>    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente adipisci soluta at itaque, aspernatur minus
-                    numquam, vel fuga facere tempora eius dolorem quibusdam iste eligendi dolore? Natus quis ut aut? Lorem ipsum dolor
-                    sit amet consectetur adipisicing elit. Aspernatur quisquam dolore est alias, reprehenderit voluptate ipsa, non quo
-                    quaerat dicta debitis! Ea fugit id veritatis dolore. Consequuntur veniam assumenda ab? Lorem ipsum, dolor sit amet
-                    consectetur adipisicing elit. Aperiam sequi autem rerum alias totam aliquid explicabo nam? A autem quibusdam
-                    suscipit praesentium cum, doloribus sit mollitia quae dicta. Tempore, ut.</p>
-            </div>
-        </div>
-        <div className="sercvices-area" id="sercvices">
-            <div className="info">
-                <h1>Services</h1>
-                <p className='info-p'>    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente adipisci soluta at itaque, aspernatur minus
-                    numquam, vel fuga facere tempora eius dolorem quibusdam iste eligendi dolore? Natus quis ut aut? Lorem ipsum dolor
-                    sit amet consectetur adipisicing elit. Aspernatur quisquam dolore est alias, reprehenderit voluptate ipsa, non quo
-                    quaerat dicta debitis! Ea fugit id veritatis dolore. Consequuntur veniam assumenda ab? Lorem ipsum, dolor sit amet
-                    consectetur adipisicing elit. Aperiam sequi autem rerum alias totam aliquid explicabo nam? A autem quibusdam
-                    suscipit praesentium cum, doloribus sit mollitia quae dicta. Tempore, ut.</p>
-            </div>
-        </div>
-        <div className="contact-area" id="contact">
-            <div className="info">
-                <h1>Contanct</h1>
-                <p className='info-p'>    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente adipisci soluta at itaque, aspernatur minus
-                    numquam, vel fuga facere tempora eius dolorem quibusdam iste eligendi dolore? Natus quis ut aut? Lorem ipsum dolor
-                    sit amet consectetur adipisicing elit. Aspernatur quisquam dolore est alias, reprehenderit voluptate ipsa, non quo
-                    quaerat dicta debitis! Ea fugit id veritatis dolore. Consequuntur veniam assumenda ab? Lorem ipsum, dolor sit amet
-                    consectetur adipisicing elit. Aperiam sequi autem rerum alias totam aliquid explicabo nam? A autem quibusdam
-                    suscipit praesentium cum, doloribus sit mollitia quae dicta. Tempore, ut.</p>
-            </div>
-        </div>
-    </div>
     )
 };
 
