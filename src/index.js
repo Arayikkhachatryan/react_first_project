8a70ff8840866fc9676c5245b3fe6a8dd28a8e46
arayikkhachatryan <solnce223@gmail.com>
2022-07-13 20:20:53 +0400

    edited stucture

--- src/index.js
+++ src/index.js
@@ -3,16 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Header from './components/header/Header'
-import Footer from './components/footer/Footer'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Header />
+  
+    
     <App />
-    <Footer />
-  </React.StrictMode>
+    
+ 
 );
 
 // If you want to start measuring performance in your app, pass a function
